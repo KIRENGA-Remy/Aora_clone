87749030da4e62cb7058b83bfe1a3aa32a89df13
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-03-06 16:22:52 -0700

    solved error on App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import "./global.css";
-import Tabs from './src/tabs'
+import Tabs from './src/tabs/_layout'
 
 export default function App() {
   return (
